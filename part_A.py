45aaf8b9f8986194d49b5528c7aa09e364ab1fde
EricNafstad <eric.nafstad@student.nhh.no>
2024-09-30 15:25:03 +0200

    Initial solution

--- part_A.py
+++ part_A.py
@@ -3,6 +3,7 @@ TECH2 mandatory assignment - Part A
 
 Write the implementation of part A of the exercise below.
 """
+x = [1, 2, 3, 4, 5]
 
 def std_loops(x):
     """
@@ -17,6 +18,26 @@ def std_loops(x):
     sd : float
         Standard deviation of the list of numbers.
     """
+    total = 0
+    count = 0
+    for num in x:
+        count += 1
+        total += num
+    average = total / count
+    
+    squared_values = []
+    total = 0
+    count = 0
+    for i in x:
+        num = i**2
+        squared_values.append(num)
+        count += 1
+        total += num
+    average2 = total / count
+    dev = (average2 - (average**2)) ** 0.5
+    return dev
+std_loops(x)
+
 
 def std_builtin(x):
     """
@@ -33,3 +54,18 @@ def std_builtin(x):
         Standard deviation of the list of numbers.
     """
     
+    meanx = (1/len(x)) * (sum(x))
+    means = sum([(num - meanx) ** 2 for num in x])
+    dev = (means / len(x)) ** 0.5
+    return dev
+std_builtin(x)
+    
+
+
+
+
+    
+import numpy as np
+x = [1, 2, 3, 4, 5]
+std_dev = np.std(x)
+print(std_dev)
